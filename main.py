e20f938d944848bb840b5c53e072cd9c53707e8a
Sergey Kospanov <sergey.kospanov@gmail.com>
2019-02-02 16:07:44 +0200

    it's 2019!

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templat
 
 def main():
     # calculate data
-    year = int(input("Year [2018]: ") or 2018)
+    year = int(input("Year [2019]: ") or 2019)
     month = int(input("Month [1]: ") or 1)
 
     try:
